95ea0cc9f93494889271b4946da8560c4607553c
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-01-09 20:11:36 -0300

    Add cliente_create

--- project/apps/cliente/views.py
+++ project/apps/cliente/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
 from . import models
-
+from . import forms
 
 def index(request):
     return render(request, "cliente/index.html")
@@ -16,3 +17,13 @@ def cliente_list(request):
     clientes = models.Cliente.objects.all()
     context = {"clientes": clientes}
     return render(request, "cliente/cliente_list.html", context)
+
+def cliente_create(request) -> HttpResponse:
+    if request.method == "POST":
+        form = forms.ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("cliente:cliente_list")
+    else:  # if request.method == "GET":
+        form = forms.ClienteForm()
+    return render(request, "cliente/cliente_create.html", {"form": form})
